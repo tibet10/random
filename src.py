41368da614638f77c7b4d5a4f2a62dee752ba0a2
Tashi Topgyal <ttopgyal@Tserings-MacBook-Pro.local>
2020-12-24 23:48:04 -0500

    firt commit

---
+++ src.py
@@ -0,0 +1,50 @@
+from tkinter import *
+import pyautogui, sys, random, threading, sched, time
+from pynput import mouse
+
+isMoving = True  
+
+def on_click(x, y, button, pressed):
+    global isMoving;
+    if pressed == True:
+        isMoving = False
+
+def moveMouse():
+    global isMoving
+    if isMoving:  
+        pyautogui.moveTo(random.randint(1,1000), random.randint(1,1000), 2, pyautogui.easeInQuad)
+
+    root.after(1000, moveMouse)
+
+def start():
+    global isMoving
+    isMoving = True
+
+def stop():
+    global isMoving
+    isMoving = False
+
+
+## Main ##
+
+listener = mouse.Listener(
+            on_click=on_click)
+
+listener.start()
+
+root = Tk()
+root.title("Title")
+root.geometry("500x500")
+
+app = Frame(root)
+app.grid()
+
+start = Button(app, text="Start mouse move...", command=start)
+
+start.grid()
+
+root.after(1000, moveMouse)  
+root.mainloop()
+
+
+    
